1bf998977156178e87aa7beac7d281d0420f30e8
Breno Uchoa <uchoa.breno2016@gmail.com>
2025-12-09 19:18:35 -0400

    atualizações solicitadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ProtectedRoute } from "@/components/ProtectedRoute";
 
@@ -38,30 +38,39 @@ const App = () => (
       <BrowserRouter>
         <AuthProvider>
           <Routes>
+
+            {/* Agora a raiz abre normalmente sem redirect */}
+            <Route path="/" element={<Auth />} />
+
             <Route path="/auth" element={<Auth />} />
-            <Route path="/" element={<Navigate to="/painel" replace />} />
             <Route path="/painel" element={<ProtectedRoute><Painel /></ProtectedRoute>} />
+
             <Route path="/alunos" element={<ProtectedRoute><Alunos /></ProtectedRoute>} />
             <Route path="/alunos/novo" element={<ProtectedRoute><NovoAluno /></ProtectedRoute>} />
             <Route path="/alunos/:id" element={<ProtectedRoute><PerfilAluno /></ProtectedRoute>} />
             <Route path="/alunos/:id/editar" element={<ProtectedRoute><NovoAluno /></ProtectedRoute>} />
+
             <Route path="/professores" element={<ProtectedRoute><Professores /></ProtectedRoute>} />
             <Route path="/professores/novo" element={<ProtectedRoute><NovoProfessor /></ProtectedRoute>} />
             <Route path="/professores/:id" element={<ProtectedRoute><PerfilProfessor /></ProtectedRoute>} />
             <Route path="/professores/:id/editar" element={<ProtectedRoute><NovoProfessor /></ProtectedRoute>} />
+
             <Route path="/turmas" element={<ProtectedRoute><Turmas /></ProtectedRoute>} />
             <Route path="/turmas/:turmaId/notas" element={<ProtectedRoute><Notas /></ProtectedRoute>} />
             <Route path="/turmas/:turmaId/frequencia" element={<ProtectedRoute><Frequencia /></ProtectedRoute>} />
             <Route path="/turmas/:turmaId/ata-final" element={<ProtectedRoute><AtaFinal /></ProtectedRoute>} />
+
             <Route path="/equipe-gestora" element={<ProtectedRoute><EquipeGestora /></ProtectedRoute>} />
             <Route path="/equipe-gestora/novo" element={<ProtectedRoute><NovoMembro /></ProtectedRoute>} />
             <Route path="/equipe-gestora/:id" element={<ProtectedRoute><PerfilMembro /></ProtectedRoute>} />
             <Route path="/equipe-gestora/:id/editar" element={<ProtectedRoute><NovoMembro /></ProtectedRoute>} />
+
             <Route path="/horario" element={<ProtectedRoute><Horario /></ProtectedRoute>} />
             <Route path="/calendario" element={<ProtectedRoute><Calendario /></ProtectedRoute>} />
             <Route path="/diario-digital" element={<ProtectedRoute><DiarioDigital /></ProtectedRoute>} />
             <Route path="/relatorios" element={<ProtectedRoute><Relatorios /></ProtectedRoute>} />
             <Route path="/configuracoes" element={<ProtectedRoute><Configuracoes /></ProtectedRoute>} />
+
             <Route path="*" element={<NotFound />} />
           </Routes>
         </AuthProvider>
